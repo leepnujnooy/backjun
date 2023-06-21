594a5a5dc00ed87bdb69dbca0d9f39e3dd3fb508
Yoon Jun Pil <galaxy160102@gmail.com>
2023-06-21 21:53:04 +0900

    P15663-4 N과 M DFS

---
+++ src/main/java/org/example/Silver/P15664.java
@@ -0,0 +1,70 @@
+package org.example.Silver;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class P15664 {
+
+
+
+    static StringBuilder sb;
+    static int n,m;
+    static boolean[] isVisit;
+    static int[] temp;
+    static int[] num;
+    static String str = "";
+    static String tempLine = "";
+
+    void DFS(int depth){
+        if(depth == m){
+            for (int i : temp) {
+                tempLine += i + " ";
+            }
+            if(!str.contains(tempLine)){
+                str += tempLine + "\n";
+                tempLine = "";
+                return;
+            }
+            else{
+                tempLine = "";
+                return;
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            if(!isVisit[i]){
+                isVisit[i] = true;
+                temp[depth] = num[i];
+                DFS(depth+1);
+                for (int j = i+1; j < n; j++) {
+                    isVisit[j] = false;
+                }
+            }
+        }
+
+    }
+
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        P15664 p15663 = new P15664();
+        n = sc.nextInt();
+        m = sc.nextInt();
+        isVisit = new boolean[n];
+        sb = new StringBuilder();
+        temp = new int[m];
+        num = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            num[i] = sc.nextInt();
+        }
+
+        Arrays.sort(num);
+
+        p15663.DFS(0);
+
+        System.out.println(str);
+
+    }
+}
